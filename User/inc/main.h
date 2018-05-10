e2aafaa1916adb8be4e0f38a899a06170a90f63c
manilovq@gmail.com <manilov>
2018-05-10 16:51:33 +0300

    changed oparating mode

--- User/inc/main.h
+++ User/inc/main.h
@@ -4,15 +4,20 @@
 #include "mcu_ini.h"
 
 #include "stm32f4xx.h"
-#define RED_ON()     GPIO_SetBits(GPIOD, GPIO_Pin_14)
-#define RED_OFF()    GPIO_ResetBits(GPIOD, GPIO_Pin_14)
-#define BLUE_ON()    GPIO_SetBits(GPIOD, GPIO_Pin_15)
-#define BLUE_OFF()   GPIO_ResetBits(GPIOD, GPIO_Pin_15)
-#define GREEN_ON()   GPIO_SetBits(GPIOD, GPIO_Pin_12)
-#define GREEN_OFF()  GPIO_ResetBits(GPIOD, GPIO_Pin_12)
-#define ORANGE_ON()  GPIO_SetBits(GPIOD, GPIO_Pin_13)
-#define ORANGE_OFF() GPIO_ResetBits(GPIOD, GPIO_Pin_13)
-#define DELAY 200
+#define RED_ON()       GPIO_SetBits(GPIOD, GPIO_Pin_14)
+#define RED_OFF()      GPIO_ResetBits(GPIOD, GPIO_Pin_14)
+#define BLUE_ON()      GPIO_SetBits(GPIOD, GPIO_Pin_15)
+#define BLUE_OFF()     GPIO_ResetBits(GPIOD, GPIO_Pin_15)
+#define GREEN_ON()     GPIO_SetBits(GPIOD, GPIO_Pin_12)
+#define GREEN_OFF()    GPIO_ResetBits(GPIOD, GPIO_Pin_12)
+#define ORANGE_ON()    GPIO_SetBits(GPIOD, GPIO_Pin_13)
+#define ORANGE_OFF()   GPIO_ResetBits(GPIOD, GPIO_Pin_13)
+#define DELAY          100
+
+#define MODE_RED       0
+#define MODE_BLUE      1
+#define MODE_GREEN     2
+#define MODE_ORANGE    3
 
 #endif
 
--- User/src/main.c
+++ User/src/main.c
@@ -2,10 +2,18 @@
 
 
 uint16_t delay_count = 0;
+uint8_t Mode = 0;
+uint16_t Mode_count = 0;
+uint8_t Mode_new = 0;
+
+//------------------------------------------------
 
 void SysTick_Handler(void) { //1 ms
   if (delay_count > 0) {
 		delay_count--;
+	}
+	if (Mode_count > 0) {
+	  Mode_count--;
 	}
 }
 
@@ -22,10 +30,66 @@ int main(void) {
 	
 	LEDs_ini();
   SysTick_Config(SystemCoreClock/1000);//1 ms
+	Mode_count = DELAY;
+	Mode = MODE_RED;
 	
 	
 	while(1) {
-		
+		if (Mode == MODE_RED) {
+			if (Mode_new == 1) {
+			  Mode_count = DELAY;
+				RED_ON();
+				Mode_new = 0;
+			}
+				
+			if (Mode_count == 0) {
+				RED_OFF();
+			  Mode = MODE_BLUE;
+				Mode_new = 1;
+			}
+		}
+		else if (Mode == MODE_BLUE) {
+			if (Mode_new == 1) {
+				Mode_count = DELAY;
+			  BLUE_ON();
+				Mode_new = 0;
+			}
+			
+			if (Mode_count == 0) {
+			  Mode = MODE_GREEN;
+				Mode_count = DELAY;
+				BLUE_OFF();
+				Mode_new = 1;
+			}
+		}
+		else if (Mode == MODE_GREEN) {
+			
+			if (Mode_new == 1) {
+			  Mode_count = DELAY;
+				GREEN_ON();
+				Mode_new = 0;
+			}
+			if (Mode_count == 0) {
+				GREEN_OFF();
+			  Mode = MODE_ORANGE;
+				Mode_count = DELAY;
+				Mode_new = 1;
+			}
+		}
+		else if (Mode == MODE_ORANGE) {
+			if (Mode_new == 1) {
+			  Mode_count = DELAY;
+				ORANGE_ON();
+				Mode_new = 0;
+			}
+			if (Mode_count == 0) {
+				ORANGE_OFF();
+			  Mode = MODE_RED;
+				Mode_count = DELAY;
+				Mode_new = 1;
+			}
+		}
+/*
 		RED_OFF();
 		ORANGE_ON();
 		delay_ms(DELAY);
@@ -38,6 +102,6 @@ int main(void) {
 		RED_ON();
 		BLUE_OFF();
 		delay_ms(DELAY);
-		
+	*/	
 	}
 }
